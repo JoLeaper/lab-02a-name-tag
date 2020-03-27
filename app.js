952e7ea20f827d7f2821e9ebb879d41af4a1242a
JoLeaper <joleap123@gmail.com>
2020-03-26 17:12:27 -0700

    created button that allows nametag name to be changed

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const pTag = document.getElementById('name');
-pTag.textContent = 'Johnny Blaze';
-pTag.style.color = 'blue';
+const myButton = document.getElementById('button');
+myButton.addEventListener('click', () => {
+    const pTag = document.getElementById('name');
+
+    const nameInput = document.getElementById('name-input');
+
+    const myInput = nameInput.value;
+
+    pTag.textContent = myInput;
+});
+
+
+
+
+
+
+// myButton.addEventListener('click', changeName);
+// myButton.addEventListener('click', function() {});
+// pTag.textContent = 'Johnny Blaze';
+// pTag.style.color = 'blue';
